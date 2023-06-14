bb4d7311f5910543c97d74b12560ad9ebd4a39a1
Eeap <kdg97811@naver.com>
2023-06-14 23:05:17 +0900

    refactor: reassign main grid

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,21 @@ import NodePlane from './components/NodePlane.vue'
     <AppBar />
     <LeftDrawer />
     <v-main>
-      <v-container fluid class="fill-height">
-        <MainTop />
-        <NodePlane />
+      <v-container fluid class="container-size">
+        <v-row>
+          <v-col><MainTop /></v-col>
+        </v-row>
+        <v-row class="row-vueflow">
+          <v-col><NodePlane /></v-col>
+        </v-row>
+        <v-row>
+          <v-col><MainBottom /></v-col>
+        </v-row>
       </v-container>
     </v-main>
   </v-app>
 </template>
+
+<style>
+@import './styles/main.css';
+</style>
