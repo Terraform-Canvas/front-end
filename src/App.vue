cf3322fecf0c719019241a4bcd4cb11da256d3cf
Eeap <kdg97811@naver.com>
2023-06-14 20:57:37 +0900

    feat: add mainboard Top example

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
 import AppBar from './layouts/default/AppBar.vue'
 import LeftDrawer from './layouts/default/LeftDrawer.vue'
-
+import MainTop from './components/MainTop.vue'
+import MainBottom from './components/MainBottom.vue'
 import NodePlane from './components/NodePlane.vue'
 </script>
 
@@ -10,7 +11,10 @@ import NodePlane from './components/NodePlane.vue'
     <AppBar />
     <LeftDrawer />
     <v-main>
-      <NodePlane />
+      <v-container fluid class="fill-height">
+        <MainTop />
+        <NodePlane />
+      </v-container>
     </v-main>
   </v-app>
 </template>
