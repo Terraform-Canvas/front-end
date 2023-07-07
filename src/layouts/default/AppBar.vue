002a784ba780a5596f4d49231e2a5155769cfc38
FacerAin <ywsong.dev@kakao.com>
2023-07-07 14:50:02 +0900

    feat: update header design for readability

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,36 +1,24 @@
 <template>
-  <v-app-bar
-    color="white"
-    height="50"
-    extended
-    extension-height="30"
-  >
-    <v-responsive max-width="100">
-      <v-img src="@/assets/images/canvas-logo.jpg" width="auto" height="auto"/>
+  <v-app-bar class="header-bar">
+    <v-responsive max-width="150">
+      <v-img src="@/assets/images/canvas-logo.png" width="auto" height="auto" />
     </v-responsive>
     <v-spacer></v-spacer>
     <v-app-bar-nav-icon><v-icon>mdi-account</v-icon></v-app-bar-nav-icon>
     <template v-slot:extension>
-      <v-tabs>
-        <v-tab class="white--text text-none" href="/">main</v-tab>
+      <v-tabs class="header-tab-item" v-model="tab">
+        <v-tab href="/">main</v-tab>
       </v-tabs>
     </template>
-    <!-- text 위치 수정 필요 -->
-    <!-- <v-row>
-      <v-col>
-        <v-responsive max-width="156">
-          <v-text-field
-            bg-color="grey-lighten-2"
-            class="rounded-pill overflow-hidden"
-            density="compact"
-            hide-details
-            variant="solo"
-          ></v-text-field>
-        </v-responsive>
-      </v-col>
-    </v-row> -->
   </v-app-bar>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
+<style>
+.header-bar {
+  padding: 0rem 2rem;
+}
+.header-tab-item {
+  font-weight: bold;
+}
+</style>
