bf122dba8ab765c41bba3c5ef9e1f254d4cc54d0
Eeap <kdg97811@naver.com>
2023-07-07 16:11:59 +0900

    refactor: add dashboard main components

---
+++ src/layouts/components/AppMain.vue
@@ -0,0 +1,9 @@
+<template>
+    <Transition name="fade-transform" mode="out-in">
+        <router-view/>
+    </Transition>
+</template>
+  
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
