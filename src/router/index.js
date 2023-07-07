59dcb20ccfea2a8a160564e0827f6a73f90e646f
Eeap <kdg97811@naver.com>
2023-07-07 16:11:03 +0900

    refactor: add layout index to reuse

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,23 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Layout from "@/layouts"
 const routes = [
   {
     path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    component: Layout,
+    redirect: '/dashboard',
     children: [
       {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        path: 'dashboard',
+        name: 'dashboard',
+        component: () => import('@/views/dashboard/index'),
       },
     ],
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
   },
 ]
 
