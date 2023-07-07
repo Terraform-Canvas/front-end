de00e487d9a27bad82596a0e09a15b2a5c518c29
FacerAin <ywsong.dev@kakao.com>
2023-07-08 01:50:55 +0900

    feat: fix rightdrawer toggle

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -1,15 +1,13 @@
 <template>
   <AppBar />
   <LeftDrawer />
-  <v-navigation-drawer v-model="drawer" location="right" width="500" temporary>
-    <DrawerInput :selectedNode="selectedNode" />
-  </v-navigation-drawer>
+
   <AppMain />
 </template>
 
 <script setup>
 import AppBar from "../layouts/components/AppBar";
 import LeftDrawer from "../layouts/components/LeftDrawer";
-import DrawerInput from "../components/aws/DrawerInput.vue";
+
 import AppMain from "../layouts/components/AppMain";
 </script>
