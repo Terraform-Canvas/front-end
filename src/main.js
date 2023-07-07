2e25f159b5d13806e420f1f8277563ebf75be026
FacerAin <ywsong.dev@kakao.com>
2023-07-07 14:47:15 +0900

    feat: add reset css for global styling

--- src/main.js
+++ src/main.js
@@ -5,16 +5,17 @@
  */
 
 // Components
-import App from './App.vue'
+import App from "./App.vue";
+import "./styles/reset.css";
 
 // Composables
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
 // Plugins
-import { registerPlugins } from '@/plugins'
+import { registerPlugins } from "@/plugins";
+``;
+const app = createApp(App);
 
-const app = createApp(App)
+registerPlugins(app);
 
-registerPlugins(app)
-
-app.mount('#app')
+app.mount("#app");
