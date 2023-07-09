75b083c084606f1cc5d6d0e13b193d9c803fa3a5
Yong woo Song <ywsong.dev@kakao.com>
2023-07-09 22:19:20 +0900

    [기능] 베이스라인 디자인 개선 (#25)
    이후 개선 필요
    
    삭제 버튼 UI
    노드 병합시 위치 튕김
    노드 사이즈 조절
    RightDrawer 디자인 개선

--- src/components/aws/SecurityGroup.vue
+++ src/components/aws/SecurityGroup.vue
@@ -27,8 +27,45 @@ onNodeDragStop((nodeDragEvent) => {
 </script>
 
 <template>
-  <div>
-    SecurityGroup
-    <ResourceNodeCommon />
+  <div class="node-wrapper">
+    <div class="node-header">
+      <div class="node-title">Security Group</div>
+      <ResourceNodeCommon />
+    </div>
   </div>
 </template>
+
+<style>
+.node-wrapper {
+  display: flex;
+  flex-direction: column;
+  padding-left: 10px;
+}
+
+.node-title {
+  width: 30px;
+  height: 30px;
+  font-weight: bold;
+  margin-right: auto;
+}
+
+.node-header {
+  display: flex;
+}
+
+.node-detail-container {
+  display: flex;
+  padding-top: 10px;
+}
+.node-logo {
+  width: 30px;
+  height: 30px;
+}
+
+.node-detail {
+  width: 30px;
+  height: 30px;
+  color: gray;
+  padding-left: 30px;
+}
+</style>
