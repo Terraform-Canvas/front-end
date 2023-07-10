989f973f5304908077526089df7e7adb396b0657
FacerAin <ywsong.dev@kakao.com>
2023-07-10 09:27:31 +0900

    feat: update rightdrawer

--- src/components/aws/DrawerInput.vue
+++ src/components/aws/DrawerInput.vue
@@ -15,10 +15,27 @@ onUpdated(() => {
 </script>
 
 <template>
-  <div v-if="nodeType == 'alb'">alb</div>
-  <div v-else-if="nodeType == 'asg'">
-    <v-card class="mx-auto" title="AutoScaling Group">
+  <div v-if="nodeType == 'alb'">
+    <v-card class="mx-auto">
       <v-container>
+        <v-row class="drawer-header" align="center">
+          <div class="drawer-header-logo">
+            <v-img src="@/assets/resources/aws/alb.png" />
+          </div>
+          <div class="drawer-header-title">AWS Load Balancer</div>
+        </v-row>
+      </v-container>
+    </v-card>
+  </div>
+  <div v-else-if="nodeType == 'asg'">
+    <v-card class="mx-auto">
+      <v-container>
+        <div class="drawer-header-logo">
+          <v-img src="@/assets/resources/aws/asg.png" />
+        </div>
+        <v-row class="drawer-header" align="center">
+          <div class="drawer-header-title">Auto Scailing Group</div>
+        </v-row>
         <v-text-field
           v-model="selectedNode.data.min_size"
           color="primary"
@@ -56,14 +73,60 @@ onUpdated(() => {
       </v-container>
     </v-card>
   </div>
-  <div v-else-if="nodeType == 'ec2'">ec2</div>
-  <div v-else-if="nodeType == 'natgw'">natgw</div>
-  <div v-else-if="nodeType == 'privatesubnet'">privatesubnet</div>
-  <div v-else-if="nodeType == 'publicsubnet'">publicsubnet</div>
-  <div v-else-if="nodeType == 'sg'">sg</div>
-  <div v-else-if="nodeType == 'vpc'">
-    <v-card class="mx-auto" title="VPC">
+  <div v-else-if="nodeType == 'ec2'">
+    <v-card class="mx-auto" title="EC2">
       <v-container>
+        <v-row class="drawer-header" align="center">
+          <div class="drawer-header-logo">
+            <v-img src="@/assets/resources/aws/ec2.png" />
+          </div>
+          <div class="drawer-header-title">EC2</div>
+        </v-row>
+        <v-text-field
+          v-model="selectedNode.data.instance_type"
+          color="primary"
+          label="Instance Type"
+          variant="underlined"
+        ></v-text-field>
+      </v-container>
+    </v-card>
+  </div>
+  <div v-else-if="nodeType == 'natgw'">
+    <v-card class="mx-auto">
+      <v-container>
+        <v-row class="drawer-header" align="center">
+          <div class="drawer-header-logo">
+            <v-img src="@/assets/resources/aws/alb.png" />
+          </div>
+          <div class="drawer-header-title">Nat Gateway</div>
+        </v-row>
+      </v-container>
+    </v-card>
+  </div>
+  <div v-else-if="nodeType == 'privatesubnet'">
+    <v-card class="mx-auto">
+      <v-container>
+        <v-row class="drawer-header" align="center">
+          <div class="drawer-header-title">Private Subnet</div>
+        </v-row>
+      </v-container>
+    </v-card>
+  </div>
+  <div v-else-if="nodeType == 'publicsubnet'">
+    <v-card class="mx-auto">
+      <v-container>
+        <v-row class="drawer-header" align="center">
+          <div class="drawer-header-title">Public Subnet</div>
+        </v-row>
+      </v-container>
+    </v-card>
+  </div>
+  <div v-else-if="nodeType == 'sg'">
+    <v-card class="mx-auto">
+      <v-container>
+        <v-row class="drawer-header" align="center">
+          <div class="drawer-header-title">Security Group</div>
+        </v-row>
         <v-text-field
           v-model="selectedNode.data.name"
           color="primary"
@@ -73,4 +136,52 @@ onUpdated(() => {
       </v-container>
     </v-card>
   </div>
+  <div v-else-if="nodeType == 'vpc'">
+    <v-card class="mx-auto" title="VPC">
+      <v-container>
+        <v-text-field
+          v-model="selectedNode.data.name"
+          color="primary"
+          label="Name"
+          variant="underlined"
+        ></v-text-field>
+        <v-text-field
+          v-model="selectedNode.data.cidr"
+          color="primary"
+          label="CIDR"
+          variant="underlined"
+        ></v-text-field>
+        <v-text-field
+          v-model="selectedNode.data.public_subnet"
+          color="primary"
+          label="Public Subnet"
+          variant="underlined"
+        ></v-text-field>
+        <v-text-field
+          v-model="selectedNode.data.private_subnet"
+          color="primary"
+          label="Private Subnet"
+          variant="underlined"
+        ></v-text-field>
+      </v-container>
+    </v-card>
+  </div>
 </template>
+
+<style>
+.drawer-header {
+  padding-bottom: 3rem;
+  padding-top: 2rem;
+}
+
+.drawer-header-title {
+  font-weight: bold;
+  padding-left: 1rem;
+  font-size: 1.5rem;
+}
+
+.drawer-header-logo {
+  width: 4rem;
+  height: 4rem;
+}
+</style>
