386ee3343950f869ff8cd9c7e3503ab64e54a5a1
Eeap <kdg97811@naver.com>
2023-07-16 13:37:32 +0900

    style: del margin in MainTop

--- src/components/dashboard/MainTop.vue
+++ src/components/dashboard/MainTop.vue
@@ -2,14 +2,8 @@
 const region = "us-east-1";
 </script>
 
-<style>
-.top {
-  margin: 10px 0px 5px 5px;
-}
-</style>
-
 <template>
-  <v-sheet class="top text-center" :elevation="1" max-width="10%" rounded>
+  <v-sheet class="text-center" :elevation="1" max-width="10%" height="10%" rounded>
     <div class="region-box">{{ region }}</div>
   </v-sheet>
 </template>
